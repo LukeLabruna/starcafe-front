aa53b8982cafba7618c547d1f3250b3d305cb234
Lucas Labruna <lucaspablolabruna@gmail.com>
2024-12-11 18:44:47 +0000

    Created axios config and auth hook

---
+++ src/hook/useAuth.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react"
+import api from "../config/axiosConfig.js"
+
+const useAuth = () => {
+    const [auth, setAuth] = useState({ isAuth: false, user: null })
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const response = await api.get("/isauth")
+                setAuth({ isAuth: response.data.isAuth, user: response.data.user })
+            } catch (error) {
+                setAuth({ isAuth: false, user: null })
+            }
+        }
+        checkAuth()
+    }, [])
+
+    return auth
+}
+
+export default useAuth

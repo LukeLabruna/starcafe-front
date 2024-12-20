31f6103f81d6573b4eafc49376521e8a72af3f1f
Lucas Labruna <lucaspablolabruna@gmail.com>
2024-12-20 18:14:31 +0000

    Created login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from "./components/NavBar/NavBar.jsx"
 import Home from "./components/Home/Home.jsx"
 import ItemContainer from './components/ItemContainer/ItemContainer.jsx'
+import Login from './components/Login/Login.jsx'
 import Footer from "./components/Footer/Footer.jsx"
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/menu' element={<ItemContainer />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,61 @@
+import api from "../../config/axiosConfig.js"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+import { useState } from "react"
+
+const Login = () => {
+
+    const [bodyData, setBodyData] = useState({
+        userName: null,
+        password: null
+    })
+
+    const navigate = useNavigate()
+
+    const handleChange = (name, value) => {
+        setBodyData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await api.post("/user/login", bodyData)
+            navigate("/adminpanel")
+        } catch (error) {
+            console.log(error.response.data)
+        }
+
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit} className="loginForm">
+                <label htmlFor="userName">User Name:</label>
+
+                <input
+                    type="text"
+                    id="userName"
+                    name="userName"
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                />
+
+                <label htmlFor="password">Password:</label>
+
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={(e) => handleChange(e.target.name, e.target.value)}
+                />
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+export default Login

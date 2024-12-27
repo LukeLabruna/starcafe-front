dc4487896894fd5a09e72a10814aa2d76f061528
lucaspablolabruna <lucaspablolabruna@gmail.com>
2024-12-27 18:14:15 +0000

    Create icons

--- src/components/ItemContainer/ItemContainer.jsx
+++ src/components/ItemContainer/ItemContainer.jsx
@@ -4,42 +4,52 @@ import api from "../../config/axiosConfig.js"
 import CardItem from "../CardItem/CardItem.jsx"
 import Categories from "../Categories/Categories.jsx"
 import CardItemAdmin from "../CardItemAdmin/CardItemAdmin.jsx"
-import "./ItemContainer.css"
 import showToast from "../../utils/swetAlert.js"
+import Loading from "../Loading/Loading.jsx"
+import "./ItemContainer.css"
 
-const ItemContainer = ({title}) => {
+const ItemContainer = ({ title }) => {
 
     const { user, isAuth } = useContext(UserContext)
 
     const [products, setProducts] = useState([])
     const [category, setCategory] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchProducts = async () => {
+            setLoading(true)
             try {
                 const response = await api.get(`/products${category ? "?category=" + category : ""}`)
                 const newProduct = response.data.data
                 setProducts(newProduct)
+                setLoading(false)
             } catch (error) {
+                setLoading(false)
                 showToast("Error al obtener los productos", "error")
             }
         }
         fetchProducts()
     }, [category])
 
-  return (
-    <div className="itemContainer">
-        <h2>{title}</h2>
-        <Categories setCategory={setCategory} />
-        {products.length >= 1 
-        ? products.map( item => (
-            isAuth && user.role === "admin" && title === "Panel Administrador"
-            ? <CardItemAdmin key={item._id} {...item} />
-            : <CardItem key={item._id} {...item} />
-        ))
-        : <p>No se encontraron productos</p>
-        }
-    </div>
-  )
+    return (
+        <div className="itemContainer">
+            {loading
+                ? <Loading />
+                : <>
+                    <h2>{title}</h2>
+                    <Categories setCategory={setCategory} />
+                    {products.length >= 1
+                        ? products.map(item => (
+                            isAuth && user.role === "admin" && title === "Panel Administrador"
+                                ? <CardItemAdmin key={item._id} {...item} />
+                                : <CardItem key={item._id} {...item} />
+                        ))
+                        : <p>No se encontraron productos</p>
+                    }
+                </>
+            }
+        </div>
+    )
 }
 export default ItemContainer
